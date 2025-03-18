71818dfa8081bb13c8ffef1777b8e71cc90f4ad0
li yue <yue1.24@intl.zju.edu.cn>
2025-03-18 16:09:26 +0800

    inicialise

---
+++ practical5/Population Size.py
@@ -0,0 +1,6 @@
+#create two lists of sorted	values with the given data
+uk_countries=[57.11,3.13,1.91,5.45]
+china_province=[65.77,41.88,45.28,61.27,85.15]
+#print the lists
+print(uk_countries)
+print(china_province)
