70182dccfc68693e7fc059350fc4b0c3057c9eb0
li yue <yue1.24@intl.zju.edu.cn>
2025-03-26 15:27:22 +0800

    inicialise

--- practical6/SIR.py
+++ practical6/SIR.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 #define a variables called susceptible of Susceptible population
 susceptible=9999
-#define a variables called injected of injected population
-injected=1
+#define a variables called infected of infected population
+infected=1
 #define a variables called recovered of recovered population
 recovered=0
 #define a variables called N of total population
@@ -13,29 +13,33 @@ N=10000
 beta=0.3
 gamma=0.05
 #create three lists for bar plots
-injectedlist=[1]
+infectedlist=[1]
 recoveredlist=[0]
 susceptiblelist=[9999]
 #loop over 1000 time points
 for i in range(0,1000):
-     #think of a situation that nobody is injected
-    if injected<=0:
-       print("There is no people being infected")
-       break
-    #caculate the probability of injected people
-    a=np.random.choice(range(2),1,p=[beta*(injected/N),(1-beta*(injected/N))])
-    if a==0:
-       injected=injected+1
-    #caculate the probability of recovered people
-    for n in range(0,injected):
-        b=np.random.choice(range(2),1,p=[gamma,(1-gamma)])
-        if b ==0:
-           injected=injected-1
-           recovered=recovered+1
-    susceptible=N-injected
-    #record the injected, recovered and susceptible population at each time point
-    injectedlist.append(injected)
-    recoveredlist.append(recovered)
-    susceptiblelist.append(susceptible)
-xlabel=range(0,len(injectedlist))
-plt.plot(xlabel,injectedlist,marker='o',linestyle='-',color='b',label="injected")
+   #think of a situation that nobody is injected
+   if infected>0 and susceptible>0:
+      #caculate the probability of injected people
+      a=np.random.choice(range(2),susceptible,p=[(1-beta*(infected/N)),beta*(infected/N)]).sum()
+      #caculate the probability of recovered people
+      b=np.random.choice(range(2),infected,p=[(1-gamma),gamma]).sum()
+   else:
+      b=0
+      a=0
+   infected=infected-b+a
+   recovered=recovered+b
+   susceptible=N-infected-recovered
+   #record the infected, recovered and susceptible population at each time point
+   infectedlist.append(infected)
+   recoveredlist.append(recovered)
+   susceptiblelist.append(susceptible)
+xlabel=range(0,1001)
+plt.plot(xlabel,infectedlist,color='blue',label="infected")
+plt.plot(xlabel,recoveredlist,color='green',label="recovered")
+plt.plot(xlabel,susceptiblelist,color='yellow',label="susceptible")
+plt.xlabel("time")
+plt.ylabel("population")
+plt.title("SIR model")
+plt.legend()
+plt.show()
