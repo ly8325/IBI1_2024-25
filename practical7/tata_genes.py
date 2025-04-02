0ee6361655589aecf5347184ae214c0fff1b5c84
li yue <yue1.24@intl.zju.edu.cn>
2025-04-02 16:50:53 +0800

    inicialise

--- practical7/tata_genes.py
+++ practical7/tata_genes.py
@@ -1,5 +1,20 @@
 import re
 input = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r')
+tata=open("tata_genes.fa", "w")
+a=""
+b=""
 for line in input:
-    if re.search('TATAAAA',line):
-        print(line)
+    line = line.rstrip()
+    if '>' in line:
+        if re.search('TATAAAA',b) or re.search('TATAAAT',line) or re.search('TATATAA',line) or re.search('TATATAT',line):
+            a=re.findall(r'gene:(\S+)',header)[0]
+            tata.write(f'>{a}\n{b}\n')
+        header=line 
+        b=""
+    else:
+        b+=line   
+if re.search('TATAAAA',b) or re.search('TATAAAT',line) or re.search('TATATAA',line) or re.search('TATATAT',line):
+    a=re.findall(r'gene:(\S+)',line)[0]
+    tata.write(f'>{a}\n{b}\n')       
+tata.close()
+input.close()
