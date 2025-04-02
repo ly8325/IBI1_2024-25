1d42b5f5b152c5c6650b5e95f9f16a79d58c4a7d
li yue <yue1.24@intl.zju.edu.cn>
2025-04-02 16:59:52 +0800

    inicialise

--- practical7/largest_intron.py
+++ practical7/largest_intron.py
@@ -1,9 +1,12 @@
 #create a string variable seq
 seq='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 import re
-#find the longest intron
-intron=re.findall(r'GT.+?AG',seq)
+#find all the intron
+intron=re.findall(r'(?=(GT.+?AG))',seq)
 print(intron)
 #report the length of that intron
-intron=intron[0]
-print(len(intron))
+largest=0
+for i in range(len(intron)):
+    if len(intron[i])>largest:
+        largest=len(intron[i])
+print(largest)
