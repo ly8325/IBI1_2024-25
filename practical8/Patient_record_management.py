f8ffa15265a34941783462773cd9ed3e9903a295
li yue <yue1.24@intl.zju.edu.cn>
2025-04-08 16:43:03 +0800

    initialise

---
+++ practical8/Patient_record_management.py
@@ -0,0 +1,8 @@
+class patient (object):
+    def _init_(self,name,age,admission_date,medical_history):
+        self.name=name
+        self.age=age
+        self.admission_date=admission_date
+        self.medical_history=medical_history
+        print(f"{self.name}, Age: {self.age}, Last Admission: {self.admission_date}, History: {self.medical_history}")
+patient1=patient("Amy Green",18,"2025.4.8","heart attack")
